4af9518b4e51916a14cf8853205c9c6d9c85f9ab
Manuel Nava <navaweb@cbtis128.edu.mx>
2025-10-07 13:02:15 -0600

    Add files via upload
    subi archivos

---
+++ app_alumno/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+# clase Alumno con los campos nombre, edad y email
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    email = models.EmailField(unique=True)
+   
+   
+
+    def __str__(self):
+        return f"{self.nombre} {self.email}"
